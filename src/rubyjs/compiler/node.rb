cf33ca109dc83d39150b96692dff09a9fc8a3d42
Michael Neumann <mneumann@ntecs.de>
2008-11-10 16:18:40 +0100

    Reindent

--- src/rubyjs/compiler/node.rb
+++ src/rubyjs/compiler/node.rb
@@ -1,122 +1,118 @@
-module RubyJS
+module RubyJS; class Compiler
 
-  class Compiler
+  #
+  # Represents an abstract syntax tree (AST) node.
+  #
+  class Node
+    Mapping = {}
 
     #
-    # Represents an abstract syntax tree (AST) node.
+    # Registers a sexp type, or returns the registered sexp type.
     #
-    class Node
-      Mapping = {}
+    def self.kind(name=nil)
+      return @kind if name.nil?
+      Mapping[@kind = name] = self
+    end
 
-      #
-      # Registers a sexp type, or returns the registered sexp type.
-      #
-      def self.kind(name=nil)
-        return @kind if name.nil?
-        Mapping[@kind = name] = self
-      end
+    #
+    # Returns the registered kind of the class of object +self+.
+    #
+    def kind
+      self.class.kind
+    end
 
-      #
-      # Returns the registered kind of the class of object +self+.
-      #
-      def kind
-        self.class.kind
-      end
+    #
+    # Creates a Node for the given +compiler+ and process +sexp+.
+    #
+    def self.create(compiler, sexp)
+      sexp.shift || raise(Compiler::Error, "no sexp type")
 
-      #
-      # Creates a Node for the given +compiler+ and process +sexp+.
-      #
-      def self.create(compiler, sexp)
-        sexp.shift || raise(Compiler::Error, "no sexp type")
+      node = new(compiler)
+      args = node.consume(sexp)
 
-        node = new(compiler)
-        args = node.consume(sexp)
-
-        begin
-          if node.respond_to?(:normalize)
-            node = node.normalize(*args)
-          else
-            node.args(*args)
-          end
-        rescue ArgumentError => e
-          arity = node.method(node.respond_to?(:normalize) ? :normalize : :args).arity
-          err_msg = "%s (%s) takes %s argument(s): passed %s (%s) -- %s" % [
-            kind(), self, arity , args.size, args.inspect, e.message ]
-          raise ArgumentError, err_msg
+      begin
+        if node.respond_to?(:normalize)
+          node = node.normalize(*args)
+        else
+          node.args(*args)
         end
-
-        return node
+      rescue ArgumentError => e
+        arity = node.method(node.respond_to?(:normalize) ? :normalize : :args).arity
+        err_msg = "%s (%s) takes %s argument(s): passed %s (%s) -- %s" % [
+          kind(), self, arity , args.size, args.inspect, e.message ]
+        raise ArgumentError, err_msg
       end
 
-      def self.new_with_args(compiler, *args)
-        node = new(compiler)
-        node.args(*args)
-        return node
-      end
+      return node
+    end
 
-      def compiler
-        @compiler
-      end
+    def self.new_with_args(compiler, *args)
+      node = new(compiler)
+      node.args(*args)
+      return node
+    end
 
-      def initialize(compiler)
-        @compiler = compiler
-      end
+    def compiler
+      @compiler
+    end
 
-      #
-      # Consumes the +sexp+ translating it into an Array suitable for
-      # input to method +args+ or +normalize+.
-      #
-      def consume(sexp)
-        out = []
-        sexp.each do |s|
-          if s.kind_of?(Array)
-            #
-            # Reject +nil+, so that we can optimize out whole expressions
-            # just by returning +nil+ from sexp_to_node. 
-            #
-            v = @compiler.sexp_to_node(s)
-            out << v unless v.nil?
-          else
-            out << s
-          end
+    def initialize(compiler)
+      @compiler = compiler
+    end
+
+    #
+    # Consumes the +sexp+ translating it into an Array suitable for
+    # input to method +args+ or +normalize+.
+    #
+    def consume(sexp)
+      out = []
+      sexp.each do |s|
+        if s.kind_of?(Array)
+          #
+          # Reject +nil+, so that we can optimize out whole expressions
+          # just by returning +nil+ from sexp_to_node. 
+          #
+          v = @compiler.sexp_to_node(s)
+          out << v unless v.nil?
+        else
+          out << s
         end
-        return out
       end
+      return out
+    end
 
-      def args
-      end
+    def args
+    end
 
-      def expand_nil(obj)
-        if obj.nil? then Nil.new(@compiler) else obj end
-      end
+    def expand_nil(obj)
+      if obj.nil? then Nil.new(@compiler) else obj end
+    end
 
-      #
-      # Used for accessing/propagating state within the AST
-      #
+    #
+    # Used for accessing/propagating state within the AST
+    #
 
-      #
-      # Access dynamic state variables
-      #
-      def get(key)
-        @compiler.get(key)
-      end
+    #
+    # Access dynamic state variables
+    #
+    def get(key)
+      @compiler.get(key)
+    end
 
-      #
-      # Set dynamic state variables which propagate through the AST 
-      #
-      def set(hash, &block)
-        @compiler.set(hash, &block)
-      end
+    #
+    # Set dynamic state variables which propagate through the AST 
+    #
+    def set(hash, &block)
+      @compiler.set(hash, &block)
+    end
 
-      #
-      # Is +self+ of class +klass+? 
-      #
-      def is(klass)
-        self.is_a?(klass)
-      end
+    #
+    # Is +self+ of class +klass+? 
+    #
+    def is(klass)
+      self.is_a?(klass)
+    end
 
-    end # class Node
+  end # class Node
 
-  end # class Compiler
-
-end # module RubyJS
+end; end # class Compiler; module RubyJS
