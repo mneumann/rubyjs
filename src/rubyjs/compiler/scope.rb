e163934a7bf8bb10d335e869fa744e57e199cce6
Michael Neumann <mneumann@ntecs.de>
2008-11-10 16:55:44 +0100

    *

---
+++ src/rubyjs/compiler/scope.rb
@@ -0,0 +1,77 @@
+module RubyJS; class Compiler
+
+  class LocalVariable
+    attr_reader :name, :scope
+
+    def initialize(name, scope)
+      @name, @scope = name, scope
+    end
+  end
+
+  #
+  # Local variable scope
+  #
+  class LocalScope
+    attr_reader :node, :variables
+
+    def initialize(node, enclosing_scope=nil)
+      @node = node
+      @enclosing_scope = enclosing_scope
+      @variables = Hash.new
+    end
+
+    def find_variable(name, declare_if_not_found=false)
+      name = name.to_s
+      var = @variables[name] || (@enclosing_scope ? @enclosing_scope.find_variable(name) : nil)
+      if declare_if_not_found and var.nil?
+        declare_variable(name)
+      else
+        var
+      end
+    end
+
+    #
+    # Variables are always declared in the outer most local scope
+    #
+    def declare_variable(name)
+      name = name.to_s
+      raise if @variables[name]
+      var = LocalVariable.new(name, self)
+      @variables[name] = var
+      return var
+    end
+  end
+  
+  #
+  # Method scope used to write down called method and accessed
+  # instance methods.
+  #
+  class MethodScope
+    attr_reader :ivar_assignments, :ivar_lookups
+    attr_reader :method_calls
+
+    def initialize
+      @ivar_assignments = Set.new
+      @ivar_lookups = Set.new
+      @method_calls = Set.new
+      @super_call = false
+    end
+
+    def add_super_call
+      @super_call = true
+    end
+
+    def add_method_call(m)
+      @method_calls.add(m)
+    end
+
+    def add_ivar_lookup(ivar)
+      @ivar_lookups.add(ivar)
+    end
+
+    def add_ivar_assignment(ivar)
+      @ivar_assignments.add(ivar)
+    end
+  end
+
+end; end # class Compiler; module RubyJS
