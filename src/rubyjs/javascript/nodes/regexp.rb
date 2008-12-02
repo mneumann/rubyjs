5ce77dfe6f5472f3e77b6b9a234be91c139abb39
Michael Neumann <mneumann@ntecs.de>
2008-12-02 11:35:09 +0100

    Add Regexp nodes to unbreak compiling core

---
+++ src/rubyjs/javascript/nodes/regexp.rb
@@ -0,0 +1,28 @@
+module RubyJS; class Compiler; class Node
+
+  class RegexLiteral
+    def as_javascript
+      # TODO
+      @source.inspect
+    end
+  end
+
+  class DynamicRegex
+  end
+
+  class DynamicOnceRegex
+  end
+
+  class Match2
+  end
+
+  class Match3
+  end
+
+  class BackRef
+  end
+
+  class NthRef
+  end
+
+end; end; end # class Node; class Compiler; module RubyJS
